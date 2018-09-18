bbc17ba474a50ad436a6164a075d0e895060f9e6
Apollo IT <p987415@gmail.com>
2018-09-18 23:49:27 +0800

    Added API for price options of markets.

---
+++ app/api/api_v2/entities/price.rb
@@ -0,0 +1,24 @@
+module APIv2
+  module Entities
+    class Price < Base
+      expose :market_id, documentation: "Unique market id. It's always in the form of xxxyyy, where xxx is the base currency code, yyy is the quote currency code, e.g. 'btccny'. All available markets can be found at /api/v2/markets."
+      expose :market_name
+
+      expose :price_type
+      expose :price
+
+      private
+      def market_name
+        @market_name ||= ::Market.find(@object.market_id).name
+      end
+
+      def price
+        @price ||= if @object.price_type == 'normal'
+                     Global[@object.market_id].ticker[:last]
+                   else
+                     @object.price
+                   end
+      end
+    end
+  end
+end
--- app/api/api_v2/markets.rb
+++ app/api/api_v2/markets.rb
@@ -1,9 +1,23 @@
 module APIv2
   class Markets < Grape::API
+    helpers ::APIv2::NamedParams
 
     desc 'Get all available markets.'
     get "/markets" do
       present Market.all, with: APIv2::Entities::Market
+    end
+
+    desc 'Get price option of all markets'
+    get "/markets/prices" do
+      present Price.all, with: APIv2::Entities::Price
+    end
+
+    desc 'Get price option of specific market.'
+    params do
+      use :market
+    end
+    get "/markets/price" do
+      present Price.find_by_market_id(params[:market]), with: APIv2::Entities::Price
     end
 
   end
