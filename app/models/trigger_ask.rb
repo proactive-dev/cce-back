3e09d601f661dcce18661e681f3f6eac1afb304d
Apollo IT <p987415@gmail.com>
2018-12-16 23:09:35 +0800

    Cleanup.

--- app/models/trigger_ask.rb
+++ app/models/trigger_ask.rb
@@ -2,30 +2,4 @@ class TriggerAsk < TriggerOrder
 
   scope :matching_rule, -> { order('price ASC, created_at ASC') }
 
-  # def get_account_changes(order)
-  #   [order.volume, order.funds]
-  # end
-
-  # def hold_account
-  #   member.get_account(ask)
-  # end
-  #
-  # def expect_account
-  #   member.get_account(bid)
-  # end
-
-  # def avg_price
-  #   return ::Global::ZERO if funds_used.zero?
-  #   config.fix_number_precision(:bid, funds_received / funds_used)
-  # end
-
-  # def compute_locked
-  #   case ord_type
-  #   when 'limit'
-  #     volume
-  #   when 'market'
-  #     estimate_required_funds(Global[currency].bids) {|p, v| v}
-  #   end
-  # end
-
 end
--- app/models/trigger_bid.rb
+++ app/models/trigger_bid.rb
@@ -2,32 +2,4 @@ class TriggerBid < TriggerOrder
 
   scope :matching_rule, -> { order('price DESC, created_at ASC') }
 
-  # def get_account_changes(order)
-  #   [order.funds, order.volume]
-  # end
-
-  # def hold_account
-  #   member.get_account(bid)
-  # end
-  #
-  # def expect_account
-  #   member.get_account(ask)
-  # end
-
-  # def avg_price
-  #   return ::Global::ZERO if funds_received.zero?
-  #   config.fix_number_precision(:bid, funds_used / funds_received)
-  # end
-
-  # LOCKING_BUFFER_FACTOR = '1.1'.to_d
-  # def compute_locked
-  #   case ord_type
-  #   when 'limit'
-  #     price*volume
-  #   when 'market'
-  #     funds = estimate_required_funds(Global[currency].asks) {|p, v| p*v }
-  #     funds*LOCKING_BUFFER_FACTOR
-  #   end
-  # end
-
 end
