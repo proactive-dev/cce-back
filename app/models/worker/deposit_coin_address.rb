236e91d178b84dbae80477e75cbb43a8d9efe838
Apollo IT <p987415@gmail.com>
2019-04-03 17:39:59 +0800

    Removed unneeded code. (related to PaymentAddress.)

--- app/models/worker/deposit_coin_address.rb
+++ app/models/worker/deposit_coin_address.rb
@@ -9,34 +9,19 @@ module Worker
       result = nil
       acc.payment_address.tap do |pa|
         pa.with_lock do
-          next if pa.address.present?
+          next if pa.address.present? || acc.currency_obj.erc20?
 
-          result = if acc.currency_obj.erc20?
-                     get_eth_address(acc.member_id)
-                   else
-                     options = acc.currency == 'xrp' ? { is_admin: acc.member.admin? } : {}
-                     CoinAPI[acc.currency].new_address!(options)
-                   end
-
-          pa.update! \
-            result.extract!(:address, :secret, :tag).merge(details: result)
-
+          options = acc.currency == 'xrp' ? { is_admin: acc.member.admin? } : {}
+          result =  CoinAPI[acc.currency].new_address!(options)
+          pa.update! result.extract!(:address, :secret, :tag).merge(details: result)
         end
       end
 
       generate_erc20(acc.member_id, result) if acc.currency == 'eth' && !result.nil?
     end
 
-    def get_eth_address(member_id)
-      account = Account.find_by(member_id: member_id, currency: Currency.find_by_code('eth').id)
-      return unless account
-
-      { address: account.payment_address.address, secret: account.payment_address.secret }
-    end
-
     def generate_erc20(member_id, result)
       Currency.all.each do |currency|
-        next if currency.fiat?
         next unless currency.erc20?
 
         account = Account.find_by(member_id: member_id, currency: currency.id)
@@ -46,7 +31,6 @@ module Worker
           p_address.with_lock do
             p_address.update! \
               result.extract!(:address, :secret).merge(details: result)
-
           end
         end
       end
