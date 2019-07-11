039325a84310b94ef277b1d15f6e288b5458c442
ApolloIT <p987415@gmail.com>
2019-07-11 09:22:58 +0000

    Update configurations and assets.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190426170008) do
+ActiveRecord::Schema.define(version: 20190428090040) do
 
   create_table "account_versions", force: true do |t|
     t.integer  "member_id"
@@ -227,16 +227,6 @@ ActiveRecord::Schema.define(version: 20190426170008) do
     t.datetime "last_verify_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "k_lines", force: true do |t|
-    t.integer  "market"
-    t.datetime "start_at"
-    t.decimal  "low",      precision: 32, scale: 16
-    t.decimal  "high",     precision: 32, scale: 16
-    t.decimal  "open",     precision: 32, scale: 16
-    t.decimal  "close",    precision: 32, scale: 16
-    t.decimal  "volume",   precision: 32, scale: 16
   end
 
   create_table "lending_accounts", force: true do |t|
