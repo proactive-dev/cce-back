501249d10ee38ce9196a2d84abf451ab923d8e81
ApolloIT <p987415@gmail.com>
2019-07-18 01:02:05 +0000

    Update coins daemon to multi-threading from single-threading.

--- lib/daemons/coins.rb
+++ lib/daemons/coins.rb
@@ -11,50 +11,56 @@ require File.join(root, "config", "environment")
 running = true
 Signal.trap(:TERM) { running = false }
 
-def process_deposit(coin, deposit)
-  # Skip if transaction is processed.
-  return if PaymentTransaction::Normal.where(txid: deposit[:id]).exists?
 
-  # Skip zombie transactions (for which all addresses don't exist).
-  return if deposit[:entries].all? { |entry| PaymentAddress.get_with(coin, entry).nil? }
+def check_transactions(currency)
+  Rails.logger.debug {"Processing #{currency.code.upcase} deposits."}
+  client = currency.api
+  processed = 0
+  options = client.is_a?(CoinAPI::ETH) ? {blocks_limit: 40} : {}
+  client.each_deposit options do |deposit|
+    # Ignore deposits from admin addresses
+    next if AssetTransaction.where(tx_id: deposit[:id]).exists?
+    # Skip if transaction is processed.
+    next if PaymentTransaction::Normal.where(txid: deposit[:id]).exists?
+    # Skip zombie transactions (for which all addresses don't exist).
+    next if deposit[:entries].all? {|entry| PaymentAddress.get_with(currency, entry).nil?}
 
-  Rails.logger.info "Missed #{coin.code.upcase} transaction: #{deposit[:id]}."
+    received_at = deposit[:received_at]
+    Rails.logger.debug {"Processing deposit received at #{received_at.to_s('%Y-%m-%d %H:%M %Z')}."} if received_at
+    Rails.logger.info "Missed #{currency.code.upcase} transaction: #{deposit[:id]}."
 
-  # Immediately enqueue job.
-  AMQPQueue.enqueue :deposit_coin, { txid: deposit[:id], currency: coin.code}
-rescue StandardError => e
+    # Immediately enqueue job.
+    AMQPQueue.enqueue :deposit_coin, {txid: deposit[:id], currency: currency.code}
+
+    break if (processed += 1) >= 1000
+    Rails.logger.debug {"Processed #{processed} #{currency.code.upcase} #{'deposit'.pluralize(processed)}."}
+    # break if processed >= 100 || (received_at && received_at <= 1.hour.ago)
+  end
+  Rails.logger.debug {"Finished processing #{currency.code.upcase} deposits."}
+rescue => e
   Rails.logger.fatal e.inspect
 end
 
-while running
-  Currency.all.each do |currency|
-    begin
-      next if currency.fiat?
-      break unless running
-      Rails.logger.debug { "Processing #{currency.code.upcase} deposits." }
-      client    = currency.api
-      processed = 0
-      options   = client.is_a?(CoinAPI::ETH) ? { blocks_limit: 20 } : {}
-      client.each_deposit options do |deposit|
-        break unless running
-
-        # Ignore deposits from admin addresses
-        next if AssetTransaction.where(tx_id: deposit[:id]).exists?
-
-        received_at = deposit[:received_at]
-        Rails.logger.debug { "Processing deposit received at #{received_at.to_s('%Y-%m-%d %H:%M %Z')}." } if received_at
-
-        process_deposit(currency, deposit)
-
-        break if (processed += 1) >= 1000
-        Rails.logger.debug { "Processed #{processed} #{currency.code.upcase} #{'deposit'.pluralize(processed)}." }
-        # break if processed >= 100 || (received_at && received_at <= 1.hour.ago)
-      end
-      Rails.logger.debug { "Finished processing #{currency.code.upcase} deposits." }
-    rescue => e
-      Rails.logger.fatal e.inspect
+def create_thread currency
+  Thread.new do
+    loop do
+      check_transactions currency
+      sleep 30
     end
   end
+end
 
-  Kernel.sleep 5
+threads = []
+Currency.all.each do |currency|
+  next if currency.fiat?
+  thread = create_thread currency
+  threads << thread
+  sleep 5
+end
+
+while running
+  unless running
+    threads.each {|thread| thread.join}
+  end
+  sleep 5
 end
