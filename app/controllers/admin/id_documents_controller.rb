9eaccd90ec848f5f8867aae5bed4c0235fc359db
Apollo IT <p987415@gmail.com>
2019-10-01 03:16:59 +0000

    Update admin pages for KYC

--- app/controllers/admin/id_documents_controller.rb
+++ app/controllers/admin/id_documents_controller.rb
@@ -3,6 +3,14 @@ module Admin
     load_and_authorize_resource
 
     def index
+      @search_field = params[:search_field]
+      case @search_field
+      when 'verified'
+        @id_documents = @id_documents.where(aasm_state: 'verified')
+      when 'unverified'
+        @id_documents = @id_documents.where.not(aasm_state: 'verified')
+      else
+      end
       @id_documents = @id_documents.order(:updated_at).reverse_order.page params[:page]
     end
 
